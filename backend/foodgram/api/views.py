eda680e183c74f95924c3de77c2861144f5109e4
Николай Ревякин <revyakin98701@yandex.ru>
2022-06-18 19:02:13 +0600

    pep8 fixes

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,19 +1,12 @@
 from http import HTTPStatus
 
-from django.db.models import Sum
-from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
+from recipes.models import (Cart, FavoriteRecipe, Ingredient, Recipe,
+                            Subscribe, Tag)
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
-
-from recipes.models import (Cart, FavoriteRecipe, Ingredient, IngredientRecipe,
-                            Recipe, Subscribe, Tag)
 from users.models import User
 
 from .filters import IngredientFilter, RecipeFilter
@@ -112,46 +105,3 @@ class FavoriteViewSet(FavoriteCartViewSet):
     serializer_class = FavoriteSerializer
     queryset = FavoriteRecipe.objects.all()
     model = FavoriteRecipe
-
-
-class CartDownload(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
-
-    @staticmethod
-    def canvas_method(dictionary):
-        response = HttpResponse(content_type='application/pdf')
-        response[
-            'Content-Disposition'] = 'attachment; \
-        filename = "shopping_cart.pdf"'
-        begin_position_x, begin_position_y = 40, 650
-        sheet = canvas.Canvas(response, pagesize=A4)
-        pdfmetrics.registerFont(TTFont('Montserrat',
-                                       'data/Montserrat.ttf'))
-        sheet.setFont('Montserrat', 50)
-        sheet.setTitle('Список покупок')
-        sheet.drawString(begin_position_x,
-                         begin_position_y + 40, 'Список покупок: ')
-        sheet.setFont('FreeSans', 24)
-        for number, item in enumerate(dictionary, start=1):
-            if begin_position_y < 100:
-                begin_position_y = 700
-                sheet.showPage()
-                sheet.setFont('Montserrat', 24)
-            sheet.drawString(
-                begin_position_x,
-                begin_position_y,
-                f'{number}.  {item["ingredient__name"]} - '
-                f'{item["ingredient_total"]}'
-                f' {item["ingredient__unit"]}'
-            )
-            begin_position_y -= 30
-        sheet.showPage()
-        sheet.save()
-        return response
-
-    def download(self, request):
-        result = IngredientRecipe.objects.filter(
-            recipe__carts__user=request.user).values(
-            'ingredient__name', 'ingredient__measurement_unit').order_by(
-                'ingredient__name').annotate(ingredient_total=Sum('amount'))
-        return self.canvas_method(result)
